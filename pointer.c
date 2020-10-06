7b9c6e6a1297c42ef2f4f4bc9caecc9809ed80cd
kurtmuller-studnet <kurtmuller@mail.adelphi.edu>
2020-10-06 10:19:55 -0400

    Delete pointer.c

--- pointer.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(){
-    int a =7;
-    int *aPtr; // un-intialized Pointer
-    aPtr = &a; //Stores address of a in aPtr
-
-    printf("a = %d\naPtr = %p\n", a, aPtr);
-}
