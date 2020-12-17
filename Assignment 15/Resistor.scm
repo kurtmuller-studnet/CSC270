126f7c2521fa009234f90931e9b5176d1808f4ba
kurtmuller-studnet <kurtmuller@mail.adelphi.edu>
2020-12-17 08:36:38 -0500

    dsas

--- Assignment 15/Resistor.scm
+++ Assignment 15/Resistor.scm
@@ -1,37 +1,28 @@
-; Procedure to get equate the color of the band to a number
-(define (band bandColor)
-    (cond
-        ((eq? bandColor 'black) 0)
-        ((eq? bandColor 'brown) 1)
-        ((eq? bandColor 'red) 2)
-        ((eq? bandColor 'orange) 3)
-        ((eq? bandColor 'yellow) 4)
-        ((eq? bandColor 'green) 5)
-        ((eq? bandColor 'blue) 6)
-        ((eq? bandColor 'violet) 7)
-        ((eq? bandColor 'grey) 8)
-        ((eq? bandColor 'white) 9)
-        (else -1)
-        )
-)
+;;Condition statements for the colors in the list
+(define (band color)
+    (cond ((equal? color 'black)  0)
+          ((equal? color 'brown)  1)
+          ((equal? color 'red)    2)
+          ((equal? color 'orange) 3)
+          ((equal? color 'yellow) 4)
+          ((equal? color 'green)  5)
+          ((equal? color 'blue)   6)
+          ((equal? color 'violet) 7)
+          ((equal? color 'grey)   8)
+          ((equal? color 'white)  9)
+          (else -1)))
 
-;returns value of color coded bands
-(define (resistance colors)
-    (if (= (length colors) 4)
-        (*
-            ;calculate the power of 10
-            (expt 10 (band (car (cdr (cdr (cdr colors))))))
-            ;add values of 3 colors
-            (+
-                ;calculate first band
-                (* (band (car colors)) 100)
-                ;calculate second band
-                (* (band (car (cdr colors))) 10)
-                ;calculate third band
-                (band (car (cdr (cdr colors))))
-            )
-        )
-    )
-)
-;run this in the terminal
-;resistance '(red yellow black red)
+(define (lastElement list) 
+    (car (reverse list)))
+
+(define (resistance list)
+    (if (= (length list) 4)
+        ;;Takes the last element and get the power to 10
+        (* (expt 10 (band (lastElement list))) 
+            ;;Finds the color of each band and adds them together
+            (+  (* (band (car list)) 100)
+                (* (band (car (cdr list))) 10)
+                (band (car (cdr (cdr list))))))))
+
+(display (resistance '(red yellow black red)))
+(exit)
